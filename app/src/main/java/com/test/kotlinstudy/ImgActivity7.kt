9cb354a81d0652a54edf1b4eaedf67f7c598bbae
roncan <65841016+roncan1@users.noreply.github.com>
2022-02-16 19:29:47 +0900

    사진첩 앱1 실습코드

---
+++ app/src/main/java/com/test/kotlinstudy/ImgActivity7.kt
@@ -0,0 +1,11 @@
+package com.test.kotlinstudy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ImgActivity7 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_img7)
+    }
+}
--- app/src/main/java/com/test/kotlinstudy/MainActivity.kt
+++ app/src/main/java/com/test/kotlinstudy/MainActivity.kt
@@ -1,11 +1,80 @@
 package com.test.kotlinstudy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val image1 = findViewById<ImageView>(R.id.img_1)
+        image1.setOnClickListener{
+
+            Toast.makeText(this, "클릭완료", Toast.LENGTH_SHORT).show()
+
+            var intent = Intent(this, ImgActivity1::class.java)
+            startActivity(intent)
+        }
+
+        val image2 = findViewById<ImageView>(R.id.img_2)
+        image2.setOnClickListener{
+
+            Toast.makeText(this, "클릭완료", Toast.LENGTH_SHORT).show()
+
+            var intent = Intent(this, ImgActivity2::class.java)
+            startActivity(intent)
+        }
+
+        val image3 = findViewById<ImageView>(R.id.img_3)
+        image3.setOnClickListener{
+
+            Toast.makeText(this, "클릭완료", Toast.LENGTH_SHORT).show()
+
+            var intent = Intent(this, ImgActivity3::class.java)
+            startActivity(intent)
+        }
+
+        val image4 = findViewById<ImageView>(R.id.img_4)
+        image4.setOnClickListener{
+
+            Toast.makeText(this, "클릭완료", Toast.LENGTH_SHORT).show()
+
+            var intent = Intent(this, ImgActivity4::class.java)
+            startActivity(intent)
+        }
+
+        val image5 = findViewById<ImageView>(R.id.img_5)
+        image5.setOnClickListener{
+
+            Toast.makeText(this, "클릭완료", Toast.LENGTH_SHORT).show()
+
+            var intent = Intent(this, ImgActivity5::class.java)
+            startActivity(intent)
+        }
+
+        val image6 = findViewById<ImageView>(R.id.img_6)
+        image6.setOnClickListener{
+
+            Toast.makeText(this, "클릭완료", Toast.LENGTH_SHORT).show()
+
+            var intent = Intent(this, ImgActivity6::class.java)
+            startActivity(intent)
+        }
+
+        val image7 = findViewById<ImageView>(R.id.img_1)
+        image7.setOnClickListener{
+
+            Toast.makeText(this, "클릭완료", Toast.LENGTH_SHORT).show()
+
+            var intent = Intent(this, ImgActivity7::class.java)
+            startActivity(intent)
+        }
     }
+
 }
