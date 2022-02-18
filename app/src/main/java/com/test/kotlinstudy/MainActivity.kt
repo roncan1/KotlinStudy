2a575af2f44c0f83469d31f79af1402602dc11e8
roncan <65841016+roncan1@users.noreply.github.com>
2022-02-18 22:16:30 +0900

    주사위 앱 실습코드

--- app/src/main/java/com/test/kotlinstudy/MainActivity.kt
+++ app/src/main/java/com/test/kotlinstudy/MainActivity.kt
@@ -3,8 +3,10 @@ package com.test.kotlinstudy
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,54 +14,46 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btn1 = findViewById<ImageView>(R.id.img_1)
-        btn1.setOnClickListener{
-            val intent = Intent(this, ImageInsideActivity::class.java)
-            intent.putExtra("data","1")
-            startActivity(intent)
+        val dice1 = findViewById<ImageView>(R.id.dice1)
+        val dice2 = findViewById<ImageView>(R.id.dice2)
+
+        val btn_start = findViewById<Button>(R.id.btnDiceStart)
+        btn_start.setOnClickListener{
+
+            Toast.makeText(this, "주사위 굴리기", Toast.LENGTH_LONG).show()
+
+            val num1 = Random.nextInt(1, 6)
+            val num2 = Random.nextInt(1, 6)
+
+            if (num1 == 1) {
+                dice1.setImageResource(R.drawable.dice_1)
+            } else if (num1 == 2) {
+                dice1.setImageResource(com.test.kotlinstudy.R.drawable.dice_2)
+            } else if (num1 == 3) {
+                dice1.setImageResource(com.test.kotlinstudy.R.drawable.dice_3)
+            } else if (num1 == 4) {
+                dice1.setImageResource(com.test.kotlinstudy.R.drawable.dice_4)
+            } else if (num1 == 5) {
+                dice1.setImageResource(com.test.kotlinstudy.R.drawable.dice_5)
+            } else if (num1 == 6) {
+                dice1.setImageResource(com.test.kotlinstudy.R.drawable.dice_6)
+            }
+
+            if (num2 == 1) {
+                dice2.setImageResource(R.drawable.dice_1)
+            } else if (num1 == 2) {
+                dice2.setImageResource(com.test.kotlinstudy.R.drawable.dice_2)
+            } else if (num1 == 3) {
+                dice2.setImageResource(com.test.kotlinstudy.R.drawable.dice_3)
+            } else if (num1 == 4) {
+                dice2.setImageResource(com.test.kotlinstudy.R.drawable.dice_4)
+            } else if (num1 == 5) {
+                dice2.setImageResource(com.test.kotlinstudy.R.drawable.dice_5)
+            } else if (num1 == 6) {
+                dice2.setImageResource(com.test.kotlinstudy.R.drawable.dice_6)
+            }
         }
 
-        val btn2 = findViewById<ImageView>(R.id.img_2)
-        btn2.setOnClickListener{
-            val intent = Intent(this, ImageInsideActivity::class.java)
-            intent.putExtra("data","2")
-            startActivity(intent)
-        }
-
-        val btn3 = findViewById<ImageView>(R.id.img_3)
-        btn3.setOnClickListener{
-            val intent = Intent(this, ImageInsideActivity::class.java)
-            intent.putExtra("data","3")
-            startActivity(intent)
-        }
-
-        val btn4 = findViewById<ImageView>(R.id.img_4)
-        btn4.setOnClickListener{
-            val intent = Intent(this, ImageInsideActivity::class.java)
-            intent.putExtra("data","4")
-            startActivity(intent)
-        }
-
-        val btn5 = findViewById<ImageView>(R.id.img_5)
-        btn5.setOnClickListener{
-            val intent = Intent(this, ImageInsideActivity::class.java)
-            intent.putExtra("data","5")
-            startActivity(intent)
-        }
-
-        val btn6 = findViewById<ImageView>(R.id.img_6)
-        btn6.setOnClickListener{
-            val intent = Intent(this, ImageInsideActivity::class.java)
-            intent.putExtra("data","6")
-            startActivity(intent)
-        }
-
-        val btn7 = findViewById<ImageView>(R.id.img_7)
-        btn7.setOnClickListener{
-            val intent = Intent(this, ImageInsideActivity::class.java)
-            intent.putExtra("data","7")
-            startActivity(intent)
-        }
     }
 
 }
